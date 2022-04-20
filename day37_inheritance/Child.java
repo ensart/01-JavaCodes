00d795ffc60cb4362da2b2251e7eaeff57e1362f
Ensar <102087542+ensart@users.noreply.github.com>
2022-04-20 23:11:59 +0300

    Add files via upload

---
+++ day37_inheritance/Child.java
@@ -0,0 +1,34 @@
+package day37_inheritance;
+
+import day36_inheritance.Parent;
+
+public class Child extends Parent {
+    // bir class'i child class yaptigimizda
+    // parent class'daki constructor'a ulasmasi gerekir
+    // bu durumda parent class'daki constructor'in
+    // access modifier'i uygun bir modifier yapilmalidir
+
+    Child(){
+        super();
+        System.out.println("child class parametresiz cons");
+    }
+    Child(int s){
+        // Child class'da tum constructor'larin ilk satirina
+        // Java'nin yerlestirdigi constructor PARAMETRESIZ dir yani super()
+
+        System.out.println("Child class parametreli cons.");
+    }
+
+    Child(int sayi1, int sayi2){
+        // Eger parent class'dan parametresiz constructor'i degil de
+        // baska bir constructor'i calistirmak isterseniz
+        // bunu Child class'daki constructor'in ILK SATIRINA yazmalisiniz
+        super(sayi1,sayi2);
+        System.out.println("Child iki parametreli cons.");
+    }
+
+    public static void main(String[] args) {
+        Child child=new Child(5,8);
+    }
+
+}
---
+++ day37_inheritance/deneme.java
@@ -0,0 +1,7 @@
+package day37_inheritance;
+
+public class deneme {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
